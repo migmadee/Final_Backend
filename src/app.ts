1d077dc7a59c12927df743f01c177297db8e5e88
migmadee <migmademasherpa079@gmail.com>
2025-06-18 22:52:26 -0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import authRoutes from './routes/authRoutes'
+import eventRoutes from './routes/eventRoutes'
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // frontend origin
+    credentials: true,
+  })
+)
+
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/events', eventRoutes)
+// 🏠 Root route
+app.get('/', (req, res) => {
+  res.send(`
+    🚀 API is running...
+    🌐 Status: Online
+    
+  `)
+})
+
+export default app
